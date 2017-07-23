f82f762ac93df3380b395acffadfc585d790cf3b
Roland Abregorivas <abregorivas@gmail.com>
2017-07-23 01:47:59 -0700

    removed 'quotations on quote data

--- public/quotes.js
+++ public/quotes.js
@@ -1,115 +1,115 @@
 const quotes = [
     {
       author:"Dr. Seuss",
-      quote:'“Don\'t cry because it\'s over, smile because it happened.” ',
+      quote:'Don\'t cry because it\'s over, smile because it happened.',
     },
     {
       author:"Oscar Wilde",
-      quote:'“Be yourself; everyone else is already taken.” ',
+      quote:'Be yourself; everyone else is already taken.',
     },
     {
       author: "Albert Einstein",
-      quote: '“Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.” '
+      quote: 'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe. '
     },
     {
       author: "Frank Zappa",
-      quote: '“So many books, so little time.” '
+      quote: 'So many books, so little time.'
     },
     {
       author: "Mark Twain",
-      quote: '“If you tell the truth, you don\'t have to remember anything.” '
+      quote: 'If you tell the truth, you don\'t have to remember anything.'
     },
     {
       author: "Elburt Hubbard",
-      quote: ' “A friend is someone who knows all about you and still loves you.” '
+      quote: 'A friend is someone who knows all about you and still loves you.'
     },
     {
       author: "Mohatma Ghandi",
-      quote: '“Live as if you were to die tomorrow. Learn as if you were to live forever.” '
+      quote: 'Live as if you were to die tomorrow. Learn as if you were to live forever.'
     },
     {
       author: "Rob Siltanen",
-      quote: '“Here\'s to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They\'re not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can\'t do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.” '
+      quote: 'Here\'s to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They\'re not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can\'t do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.'
     },
     {
       author: "Ralph Waldo Emerson",
-      quote: '“To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.” '
+      quote: 'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.'
     },
     {
       author: "Mark Twain",
-      quote: '“Never put off till tomorrow what may be done day after tomorrow just as well.” '
+      quote: 'Never put off till tomorrow what may be done day after tomorrow just as well.'
     },
     {
       author: "Robert A. Heinlein",
-      quote: '“Women and cats will do as they please, and men and dogs should relax and get used to the idea.” '
+      quote: 'Women and cats will do as they please, and men and dogs should relax and get used to the idea.'
     },
     {
       author: "Paul Terry",
-      quote: '“Whenever I feel the need to exercise, I lie down until it goes away.” '
+      quote: 'Whenever I feel the need to exercise, I lie down until it goes away.'
     },
     {
       author: "Bill Watterson, The Complete Calvin and Hobbes",
-      quote: '“Reality continues to ruin my life.” '
+      quote: 'Reality continues to ruin my life.'
     },
     {
       author: "Charles M. Schulz, The Complete Peanuts, Vol. 5: 1959-1960",
-      quote: '“I love mankind ... it\'s people I can\'t stand!!” '
+      quote: 'I love mankind ... it\'s people I can\'t stand!!'
     },
     {
       author: "Albert Einstein",
-      quote: '“Never memorize something that you can look up.” '
+      quote: 'Never memorize something that you can look up. '
     },
     {
       author: "Ellen DeGeneres, Seriously... I'm Kidding",
-      quote: '“Accept who you are. Unless you\'re a serial killer.” '
+      quote: 'Accept who you are. Unless you\'re a serial killer.'
     },
     {
       author: "Yogi Berra",
-      quote: '“Always go to other people\'s funerals, otherwise they won\'t come to yours.” '
+      quote: 'Always go to other people\'s funerals, otherwise they won\'t come to yours.'
     },
     {
       author: "Sigmund Freud",
-      quote: '“One day, in retrospect, the years of struggle will strike you as the most beautiful.” '
+      quote: 'One day, in retrospect, the years of struggle will strike you as the most beautiful. '
     },
     {
       author: "Roy T. Bennett, The Light in the Heart",
-      quote: '“Be mindful. Be grateful. Be positive. Be true. Be kind.” '
+      quote: 'Be mindful. Be grateful. Be positive. Be true. Be kind.'
     },
     {
       author: "Thomas A. Edison",
-      quote: '“I have not failed. I\'ve just found 10,000 ways that won\'t work.”'
+      quote: 'I have not failed. I\'ve just found 10,000 ways that won\'t work.'
     },
     {
       author: "Albert Einstein",
-      quote: '“I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.”'
+      quote: 'I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.'
     },
     {
       author: "J.K. Rowling, Harry Potter and the Sorcerer's Stone",
-      quote: '“To the well-organized mind, death is but the next great adventure.” '
+      quote: 'To the well-organized mind, death is but the next great adventure.'
     },
     {
       author: "George Bernard Shaw",
-      quote: '“Life isn\'t about finding yourself. Life is about creating yourself.” '
+      quote: 'Life isn\'t about finding yourself. Life is about creating yourself.'
     },
     {
       author: "Albert Einstein",
-      quote: '“Try not to become a man of success. Rather become a man of value.” '
+      quote: 'Try not to become a man of success. Rather become a man of value.'
     },
     {
       author: "Herman Melville",
-      quote: '“It is better to fail in originality than to succeed in imitation.” '
+      quote: 'It is better to fail in originality than to succeed in imitation.'
     },
     {
       author: "W.P. Kinsella",
-      quote: '“Success is getting what you want, happiness is wanting what you get” '
+      quote: 'Success is getting what you want, happiness is wanting what you get'
     },
     {
       author: "Winston S. Churchill",
-      quote: '“Success is stumbling from failure to failure with no loss of enthusiasm.” '
+      quote: 'Success is stumbling from failure to failure with no loss of enthusiasm. '
     },
     {
       author: "Walt Disney Company",
-      quote: '“The way to get started is to quit talking and begin doing. ” '
+      quote: 'The way to get started is to quit talking and begin doing. '
     },
     ]
 
